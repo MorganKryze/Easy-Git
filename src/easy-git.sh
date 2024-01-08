ec60e5404e4c36e4498ce0f33649c14dbf222edf
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-08 19:15:55 +0100

    add two functions

--- src/easy-git.sh
+++ src/easy-git.sh
@@ -47,17 +47,23 @@ eg-help() {
 # Clone a repository, add the emojis to the hooks directory and [optionally] create a worktree for a branch
 eg-setup() {
     if [ -z "$1" ]; then
-        echo -e "eg: Please provide a repository URL."
-        echo -e "eg: ❌ Operation aborted. ❌"
+        echo "eg: Please provide a repository URL."
+        echo "eg: ❌ Operation aborted. ❌"
         return 1
     fi
     repo_name=$(basename -s .git "$1")
+
+    echo "eg: 🛠️ Cloning the project 🛠️"
     git clone "$1" main && cd main
+
+    echo "eg: 🛠️ Adding the emojis hooks 🛠️"
     cp ~/.gitemojis/* .git/hooks/
+
     if [ -z "$2" ]; then
         echo "eg: No branch name provided for worktree. No worktree created."
         return 1
     else
+        echo "eg: 🛠️ Creating the worktree 🛠️"
         git worktree add ../$2 $2
         cd ../$2
         echo "eg: 🎉 Successfully cloned and set up: $repo_name 🎉"
@@ -66,28 +72,120 @@ eg-setup() {
 }
 
 # Create a worktree for a branch
-eg-worktree() {
+eg-addworktree() {
     if [ -z "$1" ]; then
-        echo -e "eg: Please provide a branch name."
-        echo -e "eg: ❌ Operation aborted. ❌"
+        echo "eg: Please provide a branch name."
+        echo "eg: ❌ Operation aborted. ❌"
         return 1
     fi
 
     if [ ! -d ".git/" ]; then
-        echo -e "eg: .git directory not found. Consider moving to the directory of your clone (main)."
-        echo -e "eg: ❌ Operation aborted. ❌"
+        echo "eg: .git directory not found. Consider moving to the directory of your clone (main)."
+        echo "eg: ❌ Operation aborted. ❌"
         return 1
     fi
+    echo "eg: 🛠️ Pulling on main 🛠️"
+    git pull
+
+    echo "eg: 🛠️ Creating the worktree 🛠️"
     git worktree add ../$1 $1
     if [ ! -d "../$1" ]; then
-        echo -e "eg: The worktree has not been created. Consider verifying if the branch exists or has been misspelt."
-        echo -e "eg: ❌ Operation aborted. ❌"
+        echo "eg: The worktree has not been created. Consider verifying if the branch exists or has been misspelt."
+        echo "eg: ❌ Operation aborted. ❌"
         return 1
     else
         cd ../$1
         echo "🎉 Successfully created worktree: $1 🎉"
         return 0
     fi
+}
+
+# Remove a worktree for a branch
+eg-rmworktree() {
+    if [ -z "$1" ]; then
+        echo "eg: Please provide a branch name."
+        echo "eg: ❌ Operation aborted. ❌"
+        return 1
+    fi
+
+    if [ ! -d ".git/" ]; then
+        echo "eg: .git directory not found. Consider moving to the directory of your clone (main)."
+        echo "eg: ❌ Operation aborted. ❌"
+        return 1
+    fi
+
+    echo "eg: 🛠️ Removing the worktree 🛠️"
+    git worktree remove ../$1
+    if [ -d "../$1" ]; then
+        echo "eg: The worktree has not been removed. Consider verifying if the branch exists or has been misspelt."
+        echo "eg: ❌ Operation aborted. ❌"
+        return 1
+    else
+        echo "eg: 🎉 Successfully removed worktree: $1 🎉"
+        return 0
+    fi
+}
+
+# Create a branch and a worktree for it
+eg-addbranch() {
+    if [ -z "$1" ]; then
+        echo "eg: Please provide a branch name."
+        echo "eg: ❌ Operation aborted. ❌"
+        return 1
+    fi
+    if [ ! -d ".git/" ]; then
+        echo "eg: .git directory not found. Consider moving to the directory of your clone (main)."
+        echo "eg: ❌ Operation aborted. ❌"
+        return 1
+    fi
+
+    branch_name="$1"
+
+    echo "eg: Please choose an option:"
+    echo "eg: 1. Feature"
+    echo "eg: 2. Documentation"
+    echo "eg: 3. Tests"
+    echo "eg: 4. Bugfix"
+    echo "eg: 5. General improvements"
+
+    read -r choice"?eg: Enter your choice: " # for zsh
+    #read -p "eg: Enter your choice: " choice -n 1 -r confirm # for bash
+
+    case $choice in
+        1)
+            branch_name="feature-$1"
+            ;;
+        2)
+            branch_name="docs-$1"
+            ;;
+        3)
+            branch_name="test-$1"
+            ;;
+        4)
+            branch_name="fix-$1"
+            ;;
+        5)
+            branch_name="general-$1"
+            ;;
+        *)
+            echo "eg: Invalid choice. Please try again." 
+            echo "eg: ❌ Operation aborted. ❌"
+            return 1
+            ;;
+    esac
+
+    echo "eg: 🛠️ Creating the branch 🛠️"
+    git checkout -b "$branch_name" dev
+    git push -u origin "$branch_name"
+    git checkout main
+
+    echo "eg: 🛠️ Creatign the worktree 🛠️"
+    git worktree add "../$branch_name" "$branch_name"
+    cd "../$branch_name"
+
+    echo "eg: 🎉 Successfully created and set up branch: $branch_name 🎉"
+    return 0
+
 }
 
 # Rebase the current branch on another branch
@@ -103,10 +201,12 @@ eg-rebase() {
         return 1
     fi
 
+    echo "eg: 🛠️ Pulling latest commits on $1 🛠️"
     cd ../$1
     git pull
     cd -
 
+    echo "eg: 🛠️ Proceed to rebase 🛠️"
     read -r confirm"?eg: Are you sure you want to continue? [Y/n] " # for zsh
     #read -p "eg: Are you sure you want to continue? [Y/n] " -n 1 -r confirm # for bash
     if [[ $confirm =~ ^[Yy]$ ]]; then
@@ -127,6 +227,7 @@ eg-emojis() {
         echo -e "eg: ❌ Operation aborted. ❌"
         return 1
     fi
+    echo "eg: 🛠️ Adding emojis to .git/hooks/ 🛠️"
     cp ~/.gitemojis/* .git/hooks/
     echo "eg: 🎉 Successfully added emojis to: .git/hooks/ 🎉"
     return 0
